1eac9a2131f79318b10bba0a00c826d2880f8a5a
fitriMD <mutiara.devi027@gmail.com>
2021-06-06 23:16:51 +0700

    Install laravel & Bootstrap

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AlatMusikController;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('logout', [Auth::class, 'logout'], function () {
+    return abort(404);
+});
+
+Route::resource('produk', AlatMusikController::class)->middleware('auth');
+Route::prefix('admin')->middleware('auth')->group(function(){
+    Route::get('/produk', [AdminController::class, 'produk'])->name('admin.produk');
+});
